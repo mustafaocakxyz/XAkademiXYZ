1e3fa2b40fb9a0a0d0d5c0b55d8496438acee20c
mustafaocakxyz <mustafaocakxyz@gmail.com>
2025-07-06 23:48:22 +0300

    Added slogans

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // Dynamic Slogan Array - Elite Coaching Focused
 const slogans = [
     "Koçluk değil, yüksek standartlara sahip bir dönüşüm programı",
+    "Çocuğunuz matematik yapamıyorsa sanata yönlendirin.<br>Veya X Akademi'ye yönlendirin.",
     "Okul + Dershane + Koçluk + Özel Ders...<br> Veya sadece X Akademi",
     "Netleriniz yükselmezse %110 para iade garantisi",
-    "Her ay koçluk firması değiştirmeyin. <br> Her ay X Akademi ile çalışın."
+    "Her ay koçluk firması değiştirmeyin. <br> Sadece X Akademi ile çalışın."
 ];
 
 function animateElement(element, text = null, delay = 0) {
